f1f5dfb1d4d1be0329c144a986cc042b2e2e88cf
egor125552 <sitkosergej55@gmail.com>
2026-08-10 21:58:10 +0300

    Defer push delivery with Worker waitUntil

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,10 +1,10 @@
 import { handleRequest } from "./app";
-import type { Env } from "./platform/cloudflare";
+import type { Env, ExecutionContextLike } from "./platform/cloudflare";
 
 export { UserRealtime } from "./modules/realtime/infrastructure/user-realtime";
 
 export default {
-  fetch(request: Request, env: Env): Promise<Response> {
-    return handleRequest(request, env);
+  fetch(request: Request, env: Env, ctx: ExecutionContextLike): Promise<Response> {
+    return handleRequest(request, env, ctx);
   }
 };
